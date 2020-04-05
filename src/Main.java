fd4d60bfa85e834482c5f2638e42e4599b6dad81
lazaric <ionutbiggy@yahoo.com>
2020-04-05 19:52:11 +0300

    Data Types

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Exercitii
+//        1. Declara un int si transforma-l in string
+
+//        2. Declara un string si transforma-l in int
+
+//        3. Aduna String a cu String b si printeaza-l impreuna cu String C
+        String a = "27";
+        String b = "22.5";
+        String c = "Suma Stringurilor este:";
+        System.out.println("");
+
+    }
+}
---
+++ src/dataTypes/DataTypes.java
@@ -0,0 +1,15 @@
+package dataTypes;
+
+public class DataTypes {
+
+    //Exista primitive Data Types: byte, short, int, long, float, double, boolean si char
+    //Exista si non-primitive Data Types: String, Array si Classes
+    //Deocamdata, o sa ne concentram pe urmatoarele:
+
+    int numar = 0;
+    float numarFloat = 5.99f;
+    char character = 'd';
+    boolean result = true;
+    String text = "123abc";
+
+}
